849290246a7813b6d265d8e9d36b4b0e87b53cd8
Vladimir Azarov <vladimirazmich@gmail.com>
2025-10-03 00:26:24 +0200

    Task 2: /kernelcare/jiffies debugfs file
    Implement first part of task 2: provide a read-only debugfs file -
    /kernelcare/jiffies, which contains current value of jiffies kernel
    timer. To compile run "make -C sample/task".
    
    Signed-off-by: Vladimir Azarov <vladimirazmich@gmail.com>

--- samples/task/task.c
+++ samples/task/task.c
@@ -4,20 +4,53 @@
 
 #include <linux/init.h>
 #include <linux/printk.h>
+#include <linux/debugfs.h>
+#include <linux/jiffies.h>
 #include <linux/module.h>
 
-static int __init task1_init(void)
+static struct dentry *debugfs_root;
+static struct dentry *jiffies_file;
+
+static int show_jiffies(struct seq_file *s, void *)
 {
-	pr_debug("Hello, KernelCare!\n");
+	seq_printf(s, "%llu\n", get_jiffies_64());
 	return 0;
 }
 
-static void __exit task1_exit(void)
+static int jiffies_open(struct inode *inode, struct file *file)
 {
-	pr_debug("Unloading\n");
+	return single_open(file, show_jiffies, NULL);
 }
 
-module_init(task1_init);
-module_exit(task1_exit);
+static const struct file_operations jiffies_ops = {
+	.owner = THIS_MODULE,
+	.open = jiffies_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release
+};
+
+static void make_fs(void)
+{
+	debugfs_root = debugfs_create_dir("kernelcare", NULL);
+	jiffies_file = debugfs_create_file("jiffies", 0444, debugfs_root,
+					   NULL, &jiffies_ops);
+}
+
+static int __init task2_init(void)
+{
+	pr_debug("Loading\n");
+	make_fs();
+	return 0;
+}
+
+static void __exit task2_exit(void)
+{
+	pr_debug("Unloading\n");
+	debugfs_remove(debugfs_root);
+}
+
+module_init(task2_init);
+module_exit(task2_exit);
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Task 1");
+MODULE_DESCRIPTION("Task 2");
